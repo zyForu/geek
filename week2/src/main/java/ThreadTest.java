a295e38629ef556e7e1f8e88779445a03e53990e
zyforu <zhangyong.zh@foxmail.com>
2023-08-09 18:38:33 +0800

    20230809:jvm运行时数据区

---
+++ week2/src/main/java/ThreadTest.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zy
+ * @date 2023/8/9 14:37
+ */
+public class ThreadTest {
+    public static void recurive(double d) {
+        if(d == 0) {
+            return;
+        }
+        recurive(d-1);
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 100000; i++) {
+            new Thread("Thread-" + i) {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName());
+                    recurive(30000);
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }
+    }
+
+}
